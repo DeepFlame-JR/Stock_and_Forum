b1e26fb699579686f0384aa27650629efeaa5289
DeepFlame-JR <wnsfuf0121@naver.com>
2022-02-26 22:03:47 +0900

    chore(airflow): airflow 사전 작업 . airflow test

--- src/airflow/first_dags.py
+++ src/airflow/first_dags.py
@@ -1,19 +1,22 @@
-import datetime
+import datetime,pendulum
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 
+KST = pendulum.timezone("Asia/Seoul")
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime.datetime(2022, 2, 23),
-    'retries': 2,
+    'start_date': days_ago(1),
+    # 'start_date': datetime.datetime(2022, 2, 26, tzinfo=KST),
+    'retries': 1,
     'retry_delay': datetime.timedelta(minutes=5)
     }
 
 dag = DAG(
-    dag_id='test_task',
+    dag_id='first_dag3',
     default_args=default_args,
-    schedule_interval='@daily'
+    schedule_interval='*/1 * * * *', #mm hh
+    catchup=False,
     )
 
 test1 = BashOperator(task_id='test1', bash_command='python /shared/python/test1.py', dag=dag)
